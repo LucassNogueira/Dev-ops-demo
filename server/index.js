67e5368ec26bb652bc693cd734c9857fbf7246ef
LucassNogueira <simplylucasjason@gmail.com>
2022-02-10 19:25:04 -0600

    adding a log

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
+
+rollbar.log("Testing");
 const students = ["jimmy", "sam", "ben", "peter"];
 
 app.get("/", (req, res) => {
